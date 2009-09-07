c0a8413ac4c130df6da833d8741853071c03103c
andy.kishore <andy.kishore@98482328-9b53-11de-b131-e14334cf7d81>
2009-09-07 23:52:42 +0000

    - feed summarizer project

---
+++ core/src/dygest/feeds/summarizer/FeedSummarizer.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dygest.feeds.summarizer;
+
+import com.sun.syndication.feed.synd.SyndContent;
+import com.sun.syndication.feed.synd.SyndEntryImpl;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.SyndFeedOutput;
+import dygest.text.ScoredSentence;
+import dygest.text.summerizer.SynmanticSummerizer;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author anand
+ */
+public class FeedSummarizer {
+
+    private static SynmanticSummerizer summarizer = null;
+
+    static {
+        try {
+            summarizer = new SynmanticSummerizer();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private SyndFeed getFeed(String url) throws Exception {
+        URL feedUrl = new URL(url);
+        SyndFeedInput input = new SyndFeedInput(false);
+        SyndFeed feed = input.build(new InputStreamReader(feedUrl.openStream()));
+
+        return feed;
+    }
+
+    public String summarize(String feedURL) {
+        if(summarizer != null) {
+            try {
+                SyndFeedOutput output = new SyndFeedOutput();
+                SyndFeed feed = getFeed(feedURL);
+                List<SyndEntryImpl> entries = feed.getEntries();
+
+                for(SyndEntryImpl entry : entries) {
+                    String uri = entry.getLink();
+                    StringBuffer summary = new StringBuffer();
+                    List<ScoredSentence> sentences = summarizer.summerize(uri);
+                    int len = 0;
+                    for(ScoredSentence s : sentences) {
+                        summary.append(s.getText());
+                        len++;
+                        if(len == 4) {
+                            break;
+                        }
+                    }
+
+                    Iterator contentIter = entry.getContents().iterator();
+                    while(contentIter.hasNext()) {
+                        // Target the description node
+                        SyndContent content =
+                                (SyndContent) contentIter.next();
+
+                        // Create and set a footer-appended description
+                        content.setValue(summary.toString());
+                    }
+                }
+
+                return output.outputString(feed);
+            } catch(Exception e) {
+                e.printStackTrace();
+                // return the original feed
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String args[]) {
+        FeedSummarizer fs = new FeedSummarizer();
+        String feed = fs.summarize("http://googleblog.blogspot.com/atom.xml");
+
+        System.out.println(feed);
+    }
+
+}
